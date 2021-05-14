a4b499fa2b2d63474ed34f3d76f6528aa94a2138
Patrique Porto <patrikrock@hotmail.com>
2021-05-13 22:36:07 -0300

    Add files via upload

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,5 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all.order(:title)
-    render json: @categories
-   # render 'categories/index.json.jbuilder'
-     
+    @categories = Category.all.order(:title)   
   end
 end
