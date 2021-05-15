86771ef7a46b1742bcd3ec7e1ae9c260e323da79
Patrique Porto <patrikrock@hotmail.com>
2021-05-15 11:43:05 -0300

    Add files via upload

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,28 @@
 class OrdersController < ApplicationController
-  def create
-  end
+  before_action :set_order, only: :show
 
-  def show
+ def create
+   @order = Order.new(order_params)
+   if @order.save
+     @order
+   else
+     render json: @order.errors, status: :unprocessable_entity
+   end
+ end
+
+ def show
+ end
+
+ private
+
+ def set_order
+   @order = Order.find(params[:id])
+ end
+ 
+  def order_params
+    params.require(:order).permit(
+      :name, :phone_number, :restaurant_id, :city, :neighborhood, :street,
+      :number, :complement, order_products_attributes: [:quantity, :product_id]
+    )
   end
 end
